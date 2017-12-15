8f4183aa9a258588af7b5a230851ff9e7523c0a8
Duen <duenstorm@gmail.com>
2017-12-15 11:31:22 +0200

    testing auth

---
+++ MeetUpApp/JavaScript/init.js
@@ -0,0 +1,57 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function(m) {
+
+    // The global jQuery object is passed as a parameter
+  	m(window.jQuery, window, document);
+
+  }(function($, window, document) {
+
+    var search, searchArr, keyValue, results;
+  
+    $(function() {
+
+      // The DOM is ready!
+
+    });
+    
+    search=$(location).attr("hash");
+    search=search.replace("#","");
+    searchArr=search.split("&");
+    
+    if(searchArr.length<2){
+        $(location).attr("href", "https://secure.meetup.com/oauth2/authorize?client_id=k2a9mnqnc4mlvoggjtlhu8p24h&response_type=token&redirect_uri=https://duenstorm.github.io/MeetUpApp");
+    }
+    else{
+        keyValue=searchArr[0];
+        keyValue=keyValue.substring((keyValue.indexOf("=")+1),keyValue.length);
+        $.get("https://api.meetup.com/2/categories",{"access_token":keyValue}, function(data, status){
+            document.write(data);
+            document.write(status);
+        });
+
+/*        
+        $.ajax({
+          type: "GET",
+          beforeSend: function(request) {
+            request.setRequestHeader("Origin", "http://127.0.0.1:8383/MeetupApp/index.html");
+          },
+          url: "https://api.meetup.com/2/categories/",
+          data: "access_token="+keyValue,
+          success: function(msg) {
+            document.write(msg);
+          }
+        });
+    }
+  */  
+    
+    
+
+  }
+  ));
+
+
