8984c27283456b5361f5e8c5882cfb56eae662d3
Duen <duenstorm@gmail.com>
2017-12-21 16:53:11 +0200

    first upload of finsihed up

--- MeetUpApp/JavaScript/init.js
+++ MeetUpApp/JavaScript/init.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-(function(m) {
+(function(m) { 
 
     // The global jQuery object is passed as a parameter
   	m(window.jQuery, window, document);
@@ -13,46 +13,11 @@
 
     var search, searchArr, keyValue, results;
   
-    $(function() {
-
-      // The DOM is ready!
-
+    $(function(){
+        requests.category();
+        requests.group(0, 20,"");
     });
-    
-    search=$(location).attr("hash");
-    search=search.replace("#","");
-    searchArr=search.split("&");
-    
-    if(searchArr.length<2){
-        $(location).attr("href", "https://secure.meetup.com/oauth2/authorize?client_id=k2a9mnqnc4mlvoggjtlhu8p24h&response_type=token&redirect_uri=https://duenstorm.github.io/MeetUpApp");
-    }
-    else{
-        keyValue=searchArr[0];
-        keyValue=keyValue.substring((keyValue.indexOf("=")+1),keyValue.length);
-        $.get("https://api.meetup.com/2/categories",{"access_token":keyValue}, function(data, status){
-            document.write(data);
-            document.write(status);
-        });
-
-/*        
-        $.ajax({
-          type: "GET",
-          beforeSend: function(request) {
-            request.setRequestHeader("Origin", "http://127.0.0.1:8383/MeetupApp/index.html");
-          },
-          url: "https://api.meetup.com/2/categories/",
-          data: "access_token="+keyValue,
-          success: function(msg) {
-            document.write(msg);
-          }
-        });
-		*/
-    }
-   
-    
-    
-
+  
   }
   ));
-
 
